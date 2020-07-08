34d3f35c3410982e63067235823aa43a629dc880
Octavian <octavian.grigorescu@codaintelligence.com>
2020-07-08 11:05:28 +0300

    Data structures and algorithms exercises

---
+++ exercises/data-structures-and-algorithms/exercises.py
@@ -0,0 +1,37 @@
+# 1. Make a list of the largest or smallest N items from another list
+
+# Input:
+# N = 3
+numbers = [34, 24, 2, 56, 7, 10, -1, -34, 8, 17]
+
+# Output:
+# [56, 34, 24] and [-34, -1, 2]
+
+
+# 2. Make a list of the largest or smallest N items from another list based on a specific key.
+
+# Input:
+# N = 2, key = 'price'
+companies = [
+    {'name': 'IBM', 'shares': 100, 'price': 91.1},
+    {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+    {'name': 'FB', 'shares': 200, 'price': 21.09},
+    {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+    {'name': 'YAHOO', 'shares': 45, 'price': 16.35},
+    {'name': 'ACME', 'shares': 75, 'price': 115.65}
+]
+
+# Output:
+# [{'name': 'AAPL', 'shares': 50, 'price': 543.22}, {'name': 'ACME', 'shares': 75, 'price': 115.65}]
+# [{'name': 'YHOO', 'shares': 45, 'price': 16.35}, {'name': 'FB', 'shares': 200, 'price': 21.09}]
+
+
+# 3. Implement a Priority Queues using two OOP classses.
+class PriorityQueue:
+    # TODO
+    pass
+
+
+class Item:
+    # TODO
+    pass
