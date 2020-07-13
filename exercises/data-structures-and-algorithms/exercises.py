37694f287ce3df3f48e0544119d155a5843a0edf
Octavian <octavian.grigorescu@codaintelligence.com>
2020-07-13 14:20:28 +0300

    Data structures and algorithms exercises

--- exercises/data-structures-and-algorithms/exercises.py
+++ exercises/data-structures-and-algorithms/exercises.py
@@ -91,3 +91,40 @@ final_dict = {
 # 7. Having the below text, create two named slices that extract the two relevant phrases:
 # Do you know why are named slices useful ?
 wierd_text = "jifrheughreidyrthisisarainydaygtjighiejdenotgreatnotterrible"
+
+
+# 8. Determine the most frequently occuring items in the next sequence:
+# Hint: use collections library.
+words = [
+    'look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
+    'the', 'eyes', 'the', 'eyes', 'the', 'eyes', 'not', 'around', 'the',
+    'eyes', "don't", 'look', 'around', 'the', 'eyes', 'look', 'into',
+    'my', 'eyes', "you're", 'under'
+]
+
+# Using the solution above, find out the number of occurrences for word "eyes".
+
+
+# 9. Generate a new dictionary (as a subset of another dictionary) taking into consideration
+# only values greater than 100. Solve this exercise using an one-liner.
+initial_dictionary = {
+    "cisco": 200,
+    "juniper": 94,
+    "checkpoint": 78,
+    "paloalto": 86,
+    "fortinet": 145,
+}
+
+
+# 10. Group these entries by the date aand print the output in a nice way :).
+# Hint: read about itertools library
+rows = [
+    {'address': '5412 N CLARK', 'date': '07/01/2012'},
+    {'address': '5148 N CLARK', 'date': '07/04/2012'},
+    {'address': '5800 E 58TH', 'date': '07/02/2012'},
+    {'address': '2122 N CLARK', 'date': '07/03/2012'},
+    {'address': '5645 N RAVENSWOOD', 'date': '07/02/2012'},
+    {'address': '1060 W ADDISON', 'date': '07/02/2012'},
+    {'address': '4801 N BROADWAY', 'date': '07/01/2012'},
+    {'address': '1039 W GRANVILLE', 'date': '07/04/2012'},
+]
