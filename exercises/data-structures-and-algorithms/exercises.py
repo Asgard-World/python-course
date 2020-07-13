8cce03c370b2915a4bffb0877c71a03e0882fdb3
Octavian <octavian.grigorescu@codaintelligence.com>
2020-07-13 13:22:42 +0300

    Data structures and algorithms exercises

--- exercises/data-structures-and-algorithms/exercises.py
+++ exercises/data-structures-and-algorithms/exercises.py
@@ -35,3 +35,59 @@ class PriorityQueue:
 class Item:
     # TODO
     pass
+
+
+# 4. Find the minimum and maximum value from the dictionary below and then sort them ascending and
+# descending.
+prices = {
+    'ACME': 45.23,
+    'AAPL': 612.78,
+    'IBM': 205.55,
+    'HPQ': 37.20,
+    'FB': 10.75
+}
+
+
+# 5. Find out:
+# - the common keys from the dictionaries below
+# - the common values from the dictionaries below
+# - the keys that are in the first dictionary and are missing in the second
+#
+# Use one-liners !
+
+first = {
+    "a": 10,
+    "b": 21,
+    "c": 8,
+}
+
+second = {
+    "b": 42,
+    "d": 10,
+    "a": 5,
+}
+
+
+# 6. Create a function that generates a new dictionary from another dictionary without
+# certain keys.
+# Example:
+
+initial_dict = {
+    "apple": 10,
+    "microsoft": 23,
+    "google": 98,
+    "amazon": 46,
+    "dell": 41,
+}
+
+# I want to remove "apple" and "microsoft" =>
+final_dict = {
+    "google": 98,
+    "amazon": 46,
+    "dell": 41,
+}
+
+
+# 7. Having the below text, create two named slices that extract the two relevant phrases:
+# Do you know why are named slices useful ?
+wierd_text = "jifrheughreidyrthisisarainydaygtjighiejdenotgreatnotterrible"
