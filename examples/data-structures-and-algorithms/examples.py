20b8c9273c5422bfa6ffc21b6ca093a51d4beb8e
Octavian <octavian.grigorescu@codaintelligence.com>
2020-08-06 11:44:41 +0300

    Data structures and algorithms examples 2

--- examples/data-structures-and-algorithms/examples.py
+++ examples/data-structures-and-algorithms/examples.py
@@ -1,4 +1,5 @@
 
+from collections import deque
 
 # ----- 1.1) Unpacking a Sequence into Separate Variables ----- #
 
@@ -75,3 +76,82 @@ print(podiums)
 *trailing, current = [10, 4, 56, 23, 65, 44, 99]
 print(trailing)
 print(current)
+
+# 6.
+# It is worth noting that the star syntax can be especially useful when iterating over a
+# sequence of tuples of varying length. For example, perhaps a sequence of tagged tuples:
+
+records = [
+    ('foo', 1, 2),
+    ('bar', 'hello'),
+    ('foo', 3, 4),
+]
+
+
+def do_foo(x, y):
+    print('foo', x, y)
+
+
+def do_bar(s):
+    print('bar', s)
+
+
+for tag, *args in records:
+    if tag == 'foo':
+        do_foo(*args)
+    elif tag == 'bar':
+        do_bar(*args)
+
+# 7.
+# Star unpacking can also be useful combined with certaing kinds of string processing.
+cpe = 'cpe:2.3:o:microsoft:windows_server_2016:1607:*:*:*:*:*:*:*'
+prefix, version, type_, vendor, product, version, *others = cpe.split(':')
+print(others)
+prefix, version, type_, vendor, product, version, *_ = cpe.split(':')
+
+# 8.
+details = ['Ferrari', 89, 60, (10, 5, 2020)]
+company, *_, (*_, year) = details
+print(company)
+print(year)
+
+# 9.
+# One could imagine writing functions that perform such splitting in order to carry out
+# some kind of clever recursive algorithm. For example:
+my_list = [32, 43, 10, 54, 28, 17, 21]
+
+
+def my_sum(items):
+    head, *tail = items
+    return head + sum(tail) if tail else head
+
+
+my_sum(my_list)
+
+
+# 10.
+# Keeping a limited history is a perfect use for a collections.deque. For example, the
+# following code performs a simple text match on a sequence of lines and yields the
+# matching line along with the previous N lines of context when found:
+
+
+def search(lines, pattern, history=5):
+    previous_lines = deque(maxlen=history)
+    for line in lines:
+        if pattern in line:
+            yield line, previous_lines
+        previous_lines.append(line)
+
+
+# Example use on a file
+with open('data.txt') as f:
+    for line, prevlines in search(f, 'python', 5):
+        for pline in prevlines:
+            print(pline, end='')
+        print(line, end='')
+        print('-' * 20)
+
+# What is `end` in `print` statement ?
+# What is `deque` ?
+# What does `yield` ?
+# Can you explain what is happening in the above code ?
